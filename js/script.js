26c7f5577f293cce50f9a88c5639c5ac70c0d4e1
Karol Konopelko <karol.konopelko@gmail.com>
2023-04-17 02:19:26 +0200

    add class active for clicked link

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 'use strict';
 
 function titleClickHandler(event){
+  
   console.log('Link was clicked!');
   console.log(event)
  
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
 const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -13,8 +14,13 @@ for(let activeLink of activeLinks){
 }
 
   /* add class 'active' to the clicked link */
+  const clickedElement = this;
+console.log('clickedElement:', clickedElement)
+this.classList.add("active");
 
-  /* remove class 'active' from all articles */
+
+
+  /* [DONE] remove class 'active' from all articles */
 
 const activeArticles = document.querySelectorAll('.posts article.active');
 
